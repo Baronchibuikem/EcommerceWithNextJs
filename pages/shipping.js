111693cd079ea4c234ce467d22763eb4fbc98c08
baron <baronchibuike@gmail.com>
2021-12-23 13:17:49 +0100

    fixed eslint bug

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,16 +1,18 @@
 import { useRouter } from "next/router";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { app_store } from "../utils/Store";
 
 export default function Shipping() {
   const router = useRouter();
 
-  const { state, dispatch } = useContext(app_store);
+  const { state } = useContext(app_store);
   const { userInfo } = state;
 
-  if (!userInfo) {
-    router.push("/login?redirect=/shipping");
-  }
+  useEffect(() => {
+    if (!userInfo) {
+      router.push("/login?redirect=/shipping");
+    }
+  }, []);
 
   return <div>shipping page</div>;
 }
