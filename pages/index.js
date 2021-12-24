8b0e35b6852e280202bf76ea765433dc617ccbc8
baron <baronchibuike@gmail.com>
2021-12-24 01:38:55 +0100

    added more logic

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,22 @@
-import { useContext } from "react";
+/* eslint-disable @next/next/no-img-element */
 import NextLink from "next/link";
-import {
-  Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Grid, Link, Typography } from "@material-ui/core";
 import Layout from "../components/Layout";
 import db from "../utils/db";
 import Product from "../models/Products";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 import { app_store } from "../utils/Store";
+import ProductItem from "../components/ProductItem";
+import Carousel from "react-material-ui-carousel";
+import useStyles from "../utils/styles";
 
 export default function Home(props) {
-  const { state, dispatch } = useContext(app_store);
+  const classes = useStyles();
   const router = useRouter();
-  const { products } = props;
-
+  const { state, dispatch } = useContext(app_store);
+  const { topRatedProducts, featuredProducts } = props;
   const addToCartHandler = async (product) => {
     const existItem = state.cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
@@ -33,56 +28,59 @@ export default function Home(props) {
     dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
     router.push("/cart");
   };
-
   return (
     <Layout>
-      <div>
-        <div>
-          <h1>Products</h1>
-          <Grid container spacing={3}>
-            {products.map((product) => (
-              <Grid item md={4} key={product.name}>
-                <Card>
-                  <NextLink href={`/product/${product.slug}`} passHref>
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        image={product.image}
-                        title={product.name}
-                      ></CardMedia>
-                      <CardContent>
-                        <Typography>{product.name}</Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </NextLink>
-                  <CardActions>
-                    <Typography>${product.price}</Typography>
-                    <Button
-                      size="small"
-                      color="primary"
-                      onClick={() => addToCartHandler(product)}
-                    >
-                      Add to Cart
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+      <Carousel className={classes.mt1} animation="slide">
+        {featuredProducts.map((product) => (
+          <NextLink
+            key={product._id}
+            href={`/product/${product.slug}`}
+            passHref
+          >
+            <Link>
+              <img
+                src={product.featuredImage}
+                alt={product.name}
+                className={classes.featuredImage}
+              ></img>
+            </Link>
+          </NextLink>
+        ))}
+      </Carousel>
+      <Typography variant="h2">Popular Products</Typography>
+      <Grid container spacing={3}>
+        {topRatedProducts.map((product) => (
+          <Grid item md={4} key={product.name}>
+            <ProductItem
+              product={product}
+              addToCartHandler={addToCartHandler}
+            />
           </Grid>
-        </div>
-      </div>
+        ))}
+      </Grid>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
   await db.connect();
-  const products = await Product.find({}).lean();
+  const featuredProductsDocs = await Product.find(
+    { isFeatured: true },
+    "-reviews"
+  )
+    .lean()
+    .limit(3);
+  const topRatedProductsDocs = await Product.find({}, "-reviews")
+    .lean()
+    .sort({
+      rating: -1,
+    })
+    .limit(6);
   await db.disconnect();
-
   return {
     props: {
-      products: products.map(db.convertDocToObj),
+      featuredProducts: featuredProductsDocs.map(db.convertDocToObj),
+      topRatedProducts: topRatedProductsDocs.map(db.convertDocToObj),
     },
   };
 }
--- utils/Store.js
+++ utils/Store.js
@@ -51,7 +51,45 @@ function reducer(state, action) {
     case "USER_LOGIN":
       return { ...state, userInfo: action.payload };
     case "USER_LOGOUT":
-      return { ...state, userInfo: null, cart: { cartItems: [] } };
+      return {
+        ...state,
+        userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: { location: {} },
+          paymentMethod: "",
+        },
+      };
+
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
+        },
+      };
+    case "SAVE_SHIPPING_ADDRESS_MAP_LOCATION":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            location: action.payload,
+          },
+        },
+      };
+    case "SAVE_PAYMENT_METHOD":
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
+      };
+    case "CART_CLEAR":
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
 
     default:
       return state;
